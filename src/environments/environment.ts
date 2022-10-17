c6af73cd69c684878f7677ee113af54fad9d959b
lauhynn <laura.hynninen@student.laurea.fi>
2022-10-18 00:12:09 +0300

    Initial Commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: 'c964d1902b35a0180da0a0385a7786c4',
+  baseURL: 'https://api.themoviedb.org/3',
+  images: 'http://images.tmdb.org/t/p',
 };
 
 /*
